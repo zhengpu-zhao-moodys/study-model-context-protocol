45cd8e4d0749801c4b6dee7f5b38f6f5570467d0
zhengpu-zhao-moodys <Zhengpu.Zhao@moodys.com>
2025-04-29 15:13:04 -0700

    init

---
+++ example/my_first_mcp_server/server.py
@@ -0,0 +1,28 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def special_add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b + 100
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}! I'm Zhengpu"
+
+
+# execute and return the stdio output
+if __name__ == "__main__":
+    print("Starting MCP server...")
+    mcp.settings.debug = True
+    mcp.settings.log_level = "DEBUG"
+    mcp.run("sse")
+    print("Server finished.")  # This might not appear if server runs correctly
